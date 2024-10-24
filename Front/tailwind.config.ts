7c7262aa5343803f62d91f907e926030b8db85fa
Pablo Fernandez <pablo.emanuelok@gmail.com>
2024-10-23 21:02:32 -0300

    Netlify9

--- Front/tailwind.config.ts
+++ Front/tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/app/**/*.{js,ts,jsx,tsx}', // Asegúrate de incluir estas rutas
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/views/**/*.{js,ts,jsx,tsx}',
   ],
   
   theme: {
