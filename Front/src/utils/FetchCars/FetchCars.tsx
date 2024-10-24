8ddb2355eb61e4e62efae6c32fa81b21f8b6a1ad
Pablo Fernandez <pablo.emanuelok@gmail.com>
2024-10-23 22:11:51 -0300

    Footer

--- Front/src/utils/FetchCars/FetchCars.tsx
+++ Front/src/utils/FetchCars/FetchCars.tsx
@@ -18,7 +18,7 @@ export default async function fetchCars(): Promise<IProduct[]> {
 
 // Función para obtener un producto por su ID
 export async function fetchProductById(_id: string): Promise<IProduct> {
-    const res = await fetch(`http://localhost:3000/products/${_id}`);
+    const res = await fetch(`https://ab-backend-iznbqeqe7a-uc.a.run.app/products/${_id}`);
     if (!res.ok) {
         throw new Error("Failed to fetch product");
     }
@@ -30,7 +30,7 @@ export async function fetchProductById(_id: string): Promise<IProduct> {
 export async function fetchDeleteId(_id: string): Promise<boolean> {
     try {
         const token = localStorage.getItem('token');
-        const res = await fetch(`http://localhost:3000/products/${_id}`, {
+        const res = await fetch(`https://ab-backend-iznbqeqe7a-uc.a.run.app/${_id}`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
