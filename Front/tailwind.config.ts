d06e4c191228b950b80f65f1957b618f0f91e349
Pablo Fernandez <pablo.emanuelok@gmail.com>
2024-10-23 21:14:55 -0300

    Netlify12

--- Front/tailwind.config.ts
+++ Front/tailwind.config.ts
@@ -1,12 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  mode: 'jit',
-  purge: [
-    './src/app/**/*.{js,ts,jsx,tsx}', // Asegúrate de incluir estas rutas
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/views/**/*.{js,ts,jsx,tsx}',
-  ],
   content: [
     './src/app/**/*.{js,ts,jsx,tsx}', // Asegúrate de incluir estas rutas
     './src/components/**/*.{js,ts,jsx,tsx}',
