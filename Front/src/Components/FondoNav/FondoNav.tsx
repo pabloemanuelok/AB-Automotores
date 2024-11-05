4fe0273cb065fe6d670c19e5702401d95bc3a6bf
Pablo Fernandez <pablo.emanuelok@gmail.com>
2024-11-04 22:24:25 -0300

    Cambios en AB

--- Front/src/Components/FondoNav/FondoNav.tsx
+++ Front/src/Components/FondoNav/FondoNav.tsx
@@ -1,34 +1,18 @@
-// components/FondoNav/FondoNav.tsx
-"use client"
-
+"use client";
+import FondoImage from "@/Assets/TableroCronos.png";
 import React from 'react';
 import Image from 'next/image';
 
-interface FondoNavProps {
-  imageUrl: string; // URL de la imagen a mostrar
-}
-
-const FondoNav: React.FC<FondoNavProps> = ({ imageUrl }) => {
+const FondoNav = () => {
   return (
-    <div className="relative  h-[260px] md:h-[450px] overflow-hidden md:mx-4">
+    <div className="relative h-[260px] md:h-[450px] overflow-hidden md:mx-4">
       <Image
-        src={imageUrl}
+        src={FondoImage}
         alt="Fondo"
-        layout="fill"
-        objectFit="cover"
-        className="transition-all duration-300 ease-in-out"
-        style={{
-          objectPosition: '50% 15%', // Ajuste para la versión de escritorio
-        }}
+        fill
+        className="object-cover transition-all duration-300 ease-in-out md:object-[50%_15%] object-center"
         quality={100}
       />
-      <style jsx>{`
-        @media (max-width: 640px) {
-          .next-image {
-            object-position: center; // Ajuste para la versión móvil
-          }
-        }
-      `}</style>
     </div>
   );
 };
